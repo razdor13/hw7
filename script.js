e85e7909db371bc38987082532792afe459c4cc4
Razdor13 <oneamaday@gmail.com>
2023-12-12 22:41:48 +0200

    done

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const array = [1, "2", 5, 3, "5", 4, 5, 6, "7"];
 function removeElement(arr, item) {
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] === item) {
-            arr.splice([i], 1);
+            arr.splice(i, 1);
             i--;
         }
     }
